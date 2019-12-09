a1756873c39576ed23d986a3f192d7e4ba8613d6
Danila Lobanov <dlobanov@axilon.ru>
2019-12-09 17:20:25 +0300

    lesson 2
    - отклонился от урока создав отдельный файл с константами
    - хорошо бы избавится от глобала и сделать персонажа классом

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
 import pygame
 
+from constans import *
+
 pygame.init()
 
-DISPLAY_WIDTH = 800
-DISPLAY_HEIGTH = 640
-NAME_WINDOW = 'My first game'
-ICON_WINDOW = 'ico.png'
 
 display = pygame.display.set_mode((DISPLAY_WIDTH, DISPLAY_HEIGTH))
 pygame.display.set_caption(NAME_WINDOW)
@@ -15,6 +13,14 @@ pygame.display.set_icon(icon)
 
 clock = pygame.time.Clock()
 
+usr_width = 60
+usr_height = 100
+usr_x = DISPLAY_WIDTH // 3
+usr_y = DISPLAY_HEIGTH -(100 + usr_height)
+
+make_jump = False
+jump_counter = 30
+
 
 def run_game():
     game = True
@@ -23,11 +29,30 @@ def run_game():
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 pygame.quit()
-                exit()
+                quit()  # or exit()
 
-        display.fill((255, 255, 255))
+        keys = pygame.key.get_pressed()
+        if keys[pygame.K_SPACE]:
+            make_jump = True
+
+            if make_jump:
+                jump()
+
+        display.fill(COLORS.WHITE)
+
+        pygame.draw.rect(display, COLORS.GREEN, (usr_x, usr_y, usr_width, usr_height))
         pygame.display.update()
-        clock.tick(60)
+        clock.tick(FPS)
+
+
+def jump():
+    global usr_y, jump_counter, make_jump
+    if jump_counter >= -30:
+        usr_y -= jump_counter / 2.5
+        jump_counter -= 1
+    else:
+        jump_counter = 30
+        make_jump = False
 
 
 if __name__ == "__main__":
---
+++ constans.py
@@ -0,0 +1,13 @@
+DISPLAY_WIDTH = 800
+DISPLAY_HEIGTH = 640
+NAME_WINDOW = 'My first game'
+ICON_WINDOW = 'ico.png'
+FPS = 60
+
+
+# ### COLORS ### #
+
+class COLORS:
+    WHITE = (255, 255, 255)
+    BLACK = (0, 0, 0)
+    GREEN = (37, 251, 1)
